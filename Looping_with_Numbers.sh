99f08b0ba09d9fdcc29536cbe35bfa14c36fe7e3
DOGAN2010 <doganbilisim2010@gmail.com>
2022-09-23 19:45:42 +0300

    added file

---
+++ Looping_with_Numbers.sh
@@ -0,0 +1,29 @@
+# Problem: 
+# Use a for loop to display the natural numbers from  to .
+
+# Input Format
+# There is no input
+# Output Format
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# .
+# .
+# .
+# .
+# .
+# 50
+# SOLUTION:
+#!/bin/bash
+number=1
+while [[ $number -lt 51 ]]
+do 
+  for num in $number
+  do
+  echo $num
+  (( number++ ))
+  done
+done
