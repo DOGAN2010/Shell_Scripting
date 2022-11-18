c34c12d521fbc4a3fbf03ec1a33ab0470ff2abb7
DOGAN2010 <doganbilisim2010@gmail.com>
2022-11-18 17:33:17 +0300

    Added new file

--- Bash/Comparing_Numbers.sh
+++ Bash/Comparing_Numbers.sh
@@ -47,7 +47,15 @@
 
 # SOLUTION:
 
-#!/bin/bash
-read -p "first number: " x
-read -p "second number: " y
-if [[ x]]
+read X
+read Y
+if [[ $X -gt $Y ]]
+then
+  echo "X is greater than Y "
+
+elif [[ $X -lt $Y ]]
+ then
+   echo "X is less than Y"
+else 
+   echo "X is equal to Y"
+fi
